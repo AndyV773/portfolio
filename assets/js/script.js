3bfd0f9aa061ac47e56e6fa03bee9bd1b9adec79
AndyV773 <ajv103@hotmail.com>
2025-06-15 15:25:49 +0100

    fix: copy codse to work on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
 const copyIcon = document.getElementsByClassName("copy-icon");
-const copyMsg = document.getElementsByClassName("copy-msg");
 
 // updates the copyright section to the most recent year
 document.getElementById("year").textContent = new Date().getFullYear();
 
-if (copyIcon && copyMsg) {
+if (copyIcon.length > 0) {
 
-    for (let buttons of copyIcon) {
-        buttons.addEventListener("click", (event) => {
+    for (let icon of copyIcon) {
+        icon.addEventListener("click", (event) => {
             const icon = event.target; // the clicked icon
             const wrapper = icon.closest(".code-block"); // find the parent container
-
             const msg = wrapper.querySelector(".copy-msg");  // scoped message
             const code = wrapper.querySelector("code");      // scoped code block
 
             // Copy code
-            navigator.clipboard.writeText(code.innerText.trim());
+            // navigator.clipboard.writeText(code.innerText.trim());
+            if (navigator.clipboard) {
+                navigator.clipboard.writeText(code.innerText.trim()).catch(err => {
+                console.error("Clipboard copy failed", err);
+                });
+            }
 
             msg.classList.add('show');
 
@@ -33,5 +36,5 @@ if (copyIcon && copyMsg) {
                 icon.style.pointerEvents = "auto";
             }, 3000); // 3 seconds
         })
-    };
+    }
 }
